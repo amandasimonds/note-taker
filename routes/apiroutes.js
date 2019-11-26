f01c7ac1694eaba03c654be48473d96e1f38d63f
Amanda Tree <amandasimonds9@gmail.com>
2019-11-26 14:29:09 -0700

    added ids to notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,25 +9,31 @@ const note = {};
 module.exports = function(app){
 
     app.get("/api/notes", function(req, res){
+
         return res.json(notes);
     });
     
-    app.get("/api/notes/:note", function(req, res){
-        var chosen = req.params.note;
-        
-        console.log(chosen);
-        
-        for( var i = 0; i < notes.length; i++){
-            if(chosen === notes[i].routeName){
-                return res.json(notes[i]);
-            }
-        }
-    })
-    
     app.post("/api/notes", function(req, res){
         var newNote = req.body;
-        newNote.routeName = newNote.title.replace("/\s+/g", "").toLowerCase();
         notes.push(newNote);
         res.json(newNote);
+        let id;
+        
+
+        for( let i = 0; i < notes.length; i++){
+            let id = notes.indexOf(notes[i]);
+            console.log(id)
+            notes[i].id = id;
+            console.log(notes);
+
+            app.delete("/api/notes/" + id, function(req, res){
+    
+            });
+        }
     });
+
+    
+
+
+    
 }
